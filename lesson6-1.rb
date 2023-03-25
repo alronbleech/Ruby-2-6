c8c83bc6036c1f4401653f1c344ed823db8dfcb3
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-03-25 13:45:12 +0000

    [Finish]2-6-1

---
+++ lesson6-1.rb
@@ -0,0 +1,12 @@
+puts "計算を始めます"
+puts "2つの値を入力してください"
+
+number_one = gets.to_i
+number_two = gets.to_i
+
+answer = number_one * number_two
+
+puts "計算結果を出力します"
+
+puts "#{number_one}*#{number_two}=#{answer}"
+puts "計算を修了します"
